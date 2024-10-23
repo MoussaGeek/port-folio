5834e9b7bd4188d86f830f4175b8f1a006822184
MoussaGeek <moussadiakite1963@gmail.com>
2024-10-23 08:31:38 +0000

    screen

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ function opentab(tabname){
 }
 
 
-var sidemeu = document.getElementById(sidemenu);
+// Sélection de l'élément "sidemenu"
+var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
-    sidemeu.style.right = "0";
+    sidemenu.style.right = "0";
 }
+
 function closemenu(){
-    sidemeu.style.right = "-200px";
+    sidemenu.style.right = "-200px";
 }
