976e58c12c43060858d3c2bd3f65cb7eb2876cd7
MoussaGeek <moussadiakite1963@gmail.com>
2024-11-02 12:18:05 +0000

    email send

--- script.js
+++ script.js
@@ -23,3 +23,51 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche la soumission classique du formulaire
+
+    // Validation des champs
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    let formIsValid = true;
+
+    if (!name || !email || !message) {
+        alert("Tous les champs sont requis.");
+        formIsValid = false;
+    }
+
+    if (formIsValid) {
+        // Affiche un message de confirmation
+        document.getElementById('confirmationMessage').style.display = 'block';
+        
+        // Réinitialiser le formulaire
+        document.getElementById('contactForm').reset();
+    }
+});
+
+function sendMail() {
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    };
+
+    const serviceID = "service_bs7bl99";
+    const templateID = "template_sjtn0u5";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("your message sent successfully");
+        })
+        .catch(err=>console.log(err));
+}
+
